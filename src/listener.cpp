f6e6ecb82758cfd23e1635e45d8e1a481bfcccc6
senthilarul <senthilhariharana@gmail.com>
2017-10-31 17:57:12 -0700

    performed cppcheck and cpplint and corrected errors

--- src/listener.cpp
+++ src/listener.cpp
@@ -25,6 +25,14 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+/**
+ * @file    listener.cpp
+ * @author  Senthil Hariharan Arul
+ * @copyright BSD
+ * @brief A ROS subscriber
+ * A ROS subscriber created from ROS listener/publisher tutorials
+ */
+
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -33,14 +41,12 @@
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
 // %Tag(CALLBACK)%
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 // %EndTag(CALLBACK)%
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
--- src/talker.cpp
+++ src/talker.cpp
@@ -24,21 +24,28 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+/**
+ * @file    talker.cpp
+ * @author  Senthil Hariharan Arul
+ * @copyright BSD
+ * @brief A ROS publisher
+ * A ROS publisher created from ROS listener/publisher tutorials
+ */
+
+// %EndTag(MSG_HEADER)%
+#include <sstream>
 // %Tag(FULLTEXT)%
 // %Tag(ROS_HEADER)%
 #include "ros/ros.h"
 // %EndTag(ROS_HEADER)%
 // %Tag(MSG_HEADER)%
 #include "std_msgs/String.h"
-// %EndTag(MSG_HEADER)%
-
-#include <sstream>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -93,8 +100,7 @@ int main(int argc, char **argv)
    */
 // %Tag(ROS_OK)%
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
 // %EndTag(ROS_OK)%
     /**
      * This is a message object. You stuff it with data, and then publish it.
