ff2164a1a269444bad8ae9ffd9291d3a24f08231
senthilarul <senthilhariharana@gmail.com>
2017-11-14 16:33:59 -0800

    modified file to correct cppcheck style warning

--- test/talkerTest.cpp
+++ test/talkerTest.cpp
@@ -38,17 +38,25 @@
  * A ROStest using gtest frame work to test talker node
  */
 
+#include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <ros/service_client.h>
-#include <gtest/gtest.h>
 #include "beginner_tutorials/modifyText.h"
 #include "std_msgs/String.h"
 
-
+/**
+ * @brief test existance of service
+ */
+TEST(testTalkerNode, testIfModifyTextExist) {
+    ros::NodeHandle nh;
+    ros::ServiceClient client = nh.serviceClient<beginner_tutorials::modifyText>("modifyText");
+    EXPECT_TRUE(client.waitForExistence(ros::Duration(5)));
+}
 /**
  * @brief  tests if modifyText service replaces default text with user input
  */
-TEST(testTalkerService, testTalkerDefaultMessageUpdate) {
+
+TEST(testTalkerNode, testTalkerDefaultMessageUpdate) {
     ros::NodeHandle nh;
     ros::ServiceClient client = nh.serviceClient<beginner_tutorials::modifyText>("modifyText");
     beginner_tutorials::modifyText::Request request;
@@ -58,4 +66,6 @@ TEST(testTalkerService, testTalkerDefaultMessageUpdate) {
     client.call(request,response);
     EXPECT_STREQ("testString", response.modifiedString.c_str());
 }
+
+
 
